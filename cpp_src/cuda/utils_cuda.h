6ac68cd3c618e06927b3c540a3951b46ff651037
zbwu1996 <zbwu1996@gmail.com>
2023-06-09 22:34:48 +0800

    Add add ifdef GPU

--- cpp_src/cuda/utils_cuda.h
+++ cpp_src/cuda/utils_cuda.h
@@ -1,6 +1,8 @@
+#ifdef GPU
 // cuda refer
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 // #include <c10/cuda/CUDAStream.h>
 // #include <c10/cuda/CUDAException.h>
+#endif
