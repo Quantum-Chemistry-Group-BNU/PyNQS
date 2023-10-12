56c313deefd11bab99976937451560519b3ab12e
zbwu1996 <zbwu1996@gmail.com>
2023-10-12 16:24:34 +0800

    add different activation-functions

--- ar_rbm.py
+++ ar_rbm.py
@@ -1,7 +1,7 @@
 # %%
 import random
 import torch
-from typing import Union, List
+from typing import Union, List, Callable
 from torch import nn, Tensor
 
 import torch.nn.functional as F
@@ -63,6 +63,8 @@ def get_cond_idx(x: Tensor):
 
 # %%
 class RBMSites(nn.Module):
+    ACTIVATION_TYPE = ("cos", "coslinear")
+
     def __init__(
         self,
         num_visible: int,
@@ -71,7 +73,9 @@ class RBMSites(nn.Module):
         init_weight: float = 0.002,
         symmetry: bool = True,
         common_weight: bool = False,
+        ar_sites: int = 2,
         device: str = "cpu",
+        activation_type="cos",
     ) -> None:
         super(RBMSites, self).__init__()
 
@@ -89,18 +93,38 @@ class RBMSites(nn.Module):
             init_weight * (torch.rand(self.num_hidden, **self.factory_kwargs) - 0.5)
         )
 
+        # Normalize one sites or two sites
+        if ar_sites not in (1, 2):
+            raise ValueError(f"ar_sites: Expected 1 or 2 but received {ar_sites}")
+        self.ar_sites = ar_sites
+
         if self.common_weight:
             length = self.num_visible
         else:
-            length = int((self.num_visible + 2) * self.num_visible * 0.5 * 0.5)
+            if self.ar_sites == 2:
+                length = int((self.num_visible + 2) * self.num_visible * 0.5 * 0.5)
+            elif self.ar_sites == 1:
+                length = int(self.num_visible * (self.num_visible + 1) * 0.5)
         self.weights = nn.Parameter(
             init_weight * (torch.rand((self.num_hidden, length), **self.factory_kwargs) - 0.5)
         )  # (Nh, length)
 
+        # Activation function types, support cos and coslinear
+        if activation_type not in self.ACTIVATION_TYPE:
+            raise TypeError(
+                f"Activate type : Expected {self.ACTIVATION_TYPE} but received {activation_type}"
+            )
+        self.activation_functions = self.activation(activation_type)
+
+        # Two-sites
         self.empty = torch.tensor([[0.0, 0.0]], **self.factory_kwargs)
         self.full = torch.tensor([[1.0, 1.0]], **self.factory_kwargs)
         self.a = torch.tensor([[1.0, 0.0]], **self.factory_kwargs)
         self.b = torch.tensor([[0.0, 1.0]], **self.factory_kwargs)
+
+        # One-sites:
+        self.occupied = torch.tensor([1.0], **self.factory_kwargs)
+        self.unoccupied = torch.tensor([0.0], **self.factory_kwargs)
 
     def effective_theta(self, x: Tensor, weights_k: Tensor) -> Tensor:
         return self.effective_theta_1(x, weights_k) + self.hidden_bias
@@ -112,20 +136,113 @@ class RBMSites(nn.Module):
     def weights_index(self, k: int) -> Tensor:
         """
         w: (nbatch, k+2), (k=2n, n = 0, 1, ...)
+        or (nbatch, k + 1), (k=0, 1, 2, ...)
         """
         if self.common_weight:
-            start = 0
-            end = k + 2
+            if self.ar_sites == 2:
+                start = 0
+                end = start + k + 2
+            elif self.ar_sites == 1:
+                start = 0
+                end = start + k + 1
         else:
-            start = int(k * (k // 2 + 1) * 0.5)
-            end = int((k + 2) * (k // 2 + 2) * 0.5)
+            if self.ar_sites == 2:
+                # (nbatch, k + 2)
+                start = int(k * (k // 2 + 1) * 0.5)
+                end = int((k + 2) * (k // 2 + 2) * 0.5)
+            elif self.ar_sites == 1:
+                # (nbatch, k + 1)
+                start = int(k * (k + 1) * 0.5)
+                end = int((k + 1) * (k + 2) * 0.5)
         return self.weights[:, start:end]
 
-    def psi(self, x: Tensor, k: int) -> Tensor:
+    @staticmethod
+    def cos_linear(x: Tensor, unit: int = 2 * torch.pi) -> Tensor:
+        x_mod = x % (2 * unit)
+        condition = x_mod < unit
+        y = torch.where(
+            condition,
+            1 - 2 / unit * (x - 2 * unit * torch.floor(x / (2 * unit))),
+            -1 + 2 / unit * (x - 2 * unit * torch.floor(x / (2 * unit)) - unit),
+        )
+        return y.to(torch.double)
+
+    def activation(self, dtype: str) -> Callable[[Tensor], Tensor]:
+        if dtype == "cos":
+            return torch.cos
+        elif dtype == "coslinear":
+            return self.cos_linear
+
+    def psi_one_sites(self, x: Tensor, k: int) -> Tensor:
+        # x: (nbatch, k)
+        nbatch = x.size(0)
+        value = torch.zeros(nbatch, 2, **self.factory_kwargs)  # (nbatch, 2)
+
+        w = self.weights_index(k)  # (Nh, k + 1)
+        theta_common = self.effective_theta(x, w[:, :k])  # (nbatch, Nh)
+        theta0 = self.effective_theta_1(self.unoccupied, w[:, k:])  # 0, (1, Nh)
+        theta1 = self.effective_theta_1(self.occupied, w[:, k:])  # 1, (1, Nh)
+
+        value[..., 0] = (self.activation_functions(theta_common + theta0)).prod(-1)
+        value[..., 1] = (self.activation_functions(theta_common + theta1)).prod(-1)
+        return F.normalize(value, dim=1, eps=1e-12)
+
+    def forward_one_sites(self, x: Tensor):
+        assert x.dim() in (1, 2)
+
+        if x.dim() == 1:
+            x = x.unsqueeze(0)
+
+        x = (x + 1) / 2  # 1/-1 -> 1/0
+        nbatch, sorb = tuple(x.size())  # (nbatch, sorb)
+
+        prob_lst: List[Tensor] = []
+        prob = torch.ones(nbatch, **self.factory_kwargs)
+
+        alpha = self.nele // 2
+        beta = self.nele // 2
+        baseline_up = alpha - self.sorb // 2
+        baseline_down = beta - self.sorb // 2
+        num_up = torch.zeros(nbatch, **self.factory_kwargs)
+        num_down = torch.zeros(nbatch, **self.factory_kwargs)
+        activations = torch.ones(nbatch, device=self.device).to(torch.bool)
+
+        for k in range(sorb):
+            x0 = x[:, :k]  # (nbatch, k)
+            y0 = self.psi_one_sites(x0, k)  # (nbatch, 2)
+
+            if self.symmetry and self.nele // 2 <= k:
+                lower_up = baseline_up + k // 2
+                lower_down = baseline_down + k // 2
+                if k % 2 == 0:
+                    activations_occ = torch.logical_and(alpha > num_up, activations)
+                    activations_unocc = torch.logical_and(lower_up < num_up, activations)
+                else:
+                    activations_occ = torch.logical_and(beta > num_down, activations)
+                    activations_unocc = torch.logical_and(lower_down < num_down, activations)
+
+                # adapt prob
+                sym_index = torch.stack([activations_unocc, activations_occ], dim=1).long()
+                y0.mul_(sym_index)
+                y0 = F.normalize(y0, dim=1, eps=1e-12)
+
+            # 0 -> [1, 0], 1 -> [0, 1]
+            index = F.one_hot(x[:, k].long(), num_classes=2).to(torch.double)
+            prob_k = (y0 * index).sum(dim=1)
+            prob.mul_(prob_k)
+            prob_lst.append(prob_k)
+
+            if k % 2 == 0:
+                num_up.add_(x[..., k])
+            else:
+                num_down.add_(x[..., k])
+        # print(torch.stack(prob_lst, dim=1))
+        return prob
+
+    def psi_two_sites(self, x: Tensor, k: int) -> Tensor:
         # x: (nbatch, k)
         nbatch = x.size(0)
         value = torch.zeros(nbatch, 4, **self.factory_kwargs)  # (nbatch, 4)
-        ax = 1.00
 
         w = self.weights_index(k)  # (Nh, k+2)
         theta_common = self.effective_theta(x, w[:, :k])  # (nbatch, Nh)
@@ -134,13 +251,13 @@ class RBMSites(nn.Module):
         theta2 = self.effective_theta_1(self.b, w[:, k:])  # 01
         theta3 = self.effective_theta_1(self.full, w[:, k:])  # 11
 
-        value[..., 0] = ax * (2 * ((theta_common + theta0).cos())).prod(-1)
-        value[..., 1] = ax * (2 * ((theta_common + theta1).cos())).prod(-1)
-        value[..., 2] = ax * (2 * ((theta_common + theta2).cos())).prod(-1)
-        value[..., 3] = ax * (2 * ((theta_common + theta3).cos())).prod(-1)
+        value[..., 0] = (self.activation_functions(theta_common + theta0)).prod(-1)
+        value[..., 1] = (self.activation_functions(theta_common + theta1)).prod(-1)
+        value[..., 2] = (self.activation_functions(theta_common + theta2)).prod(-1)
+        value[..., 3] = (self.activation_functions(theta_common + theta3)).prod(-1)
         return F.normalize(value, dim=1, eps=1e-12)
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward_two_sites(self, x: Tensor) -> Tensor:
         assert x.dim() in (1, 2)
 
         if x.dim() == 1:
@@ -164,7 +281,7 @@ class RBMSites(nn.Module):
 
         for k in range(0, sorb, 2):
             x0 = x[:, :k]  # (nbatch, k)
-            y0 = self.psi(x0, k)  # (nbatch, 4)
+            y0 = self.psi_two_sites(x0, k)  # (nbatch, 4)
 
             if self.symmetry and self.nele // 2 <= k:
                 lower_up = baseline_up + k // 2
@@ -173,7 +290,6 @@ class RBMSites(nn.Module):
                 activations_unocc0 = torch.logical_and(lower_up < num_up, activations)
                 activations_occ1 = torch.logical_and(beta > num_down, activations)
                 activations_unocc1 = torch.logical_and(lower_down < num_down, activations)
-                # breakpoint()
                 sym_index = torch.stack(
                     [activations_occ0, activations_unocc0, activations_occ1, activations_unocc1],
                     dim=1,
@@ -182,8 +298,6 @@ class RBMSites(nn.Module):
                 sym_index = constrain_make_charts(sym_index)
                 # sym_index = get_cond_idx(sym_index)
                 # assert (torch.allclose(sym_index.to(torch.double), sym_index1))
-                # breakpoint()
-                # y0 = y0 * sym_index
                 y0.mul_(sym_index)
                 y0 = F.normalize(y0, dim=1, eps=1e-12)
 
@@ -204,7 +318,52 @@ class RBMSites(nn.Module):
         return prob
 
     @torch.no_grad()
-    def ar_sampling(self, n_sample: int) -> Tensor:
+    def ar_sampling_one_sites(self, n_sample: int) ->Tensor:
+        sample = torch.zeros(n_sample, self.sorb, **self.factory_kwargs)
+
+        alpha = self.nele // 2
+        beta = self.nele // 2
+        baseline_up = alpha - self.sorb // 2
+        baseline_down = beta - self.sorb // 2
+        num_up = torch.zeros(n_sample, **self.factory_kwargs)
+        num_down = torch.zeros(n_sample, **self.factory_kwargs)
+        activations = torch.ones(n_sample, device=self.device).to(torch.bool)
+        
+        for k in range(sorb):
+            x0 = sample[:, :k]  # (nbatch, k)
+            y0 = self.psi_one_sites(x0, k)  # (nbatch, 2)
+            lower_up = baseline_up + k // 2
+            lower_down = baseline_down + k // 2
+            
+            if self.symmetry and self.nele // 2 <= k:
+                lower_up = baseline_up + k // 2
+                lower_down = baseline_down + k // 2
+                if k % 2 == 0:
+                    activations_occ = torch.logical_and(alpha > num_up, activations)
+                    activations_unocc = torch.logical_and(lower_up < num_up, activations)
+                else:
+                    activations_occ = torch.logical_and(beta > num_down, activations)
+                    activations_unocc = torch.logical_and(lower_down < num_down, activations)
+
+                # adapt prob
+                sym_index = torch.stack([activations_unocc, activations_occ], dim=1).long()
+                y0.mul_(sym_index)
+                y0 = F.normalize(y0, dim=1, eps=1e-12)
+            
+            # [0]/[1]
+            value = torch.multinomial(y0.pow(2).clamp_min(1e-12), 1).squeeze()  # (n_sample)
+            # TODO:
+            sample[:, k] = value
+            
+            if k % 2 == 0:
+                num_up.add_(value)
+            else:
+                num_down.add_(value)
+        
+        return sample
+    
+    @torch.no_grad()
+    def ar_sampling_two_sites(self, n_sample: int) -> Tensor:
         sample = torch.zeros(n_sample, self.sorb, **self.factory_kwargs)
 
         alpha = self.nele // 2
@@ -249,6 +408,18 @@ class RBMSites(nn.Module):
 
         return sample
 
+    def forward(self, x: Tensor) -> Tensor:
+        if self.ar_sites == 2:
+            return self.forward_two_sites(x)
+        elif self.ar_sites == 1:
+            return self.forward_one_sites(x)
+
+    @torch.no_grad()
+    def ar_sampling(self, n_sample: int):
+        if self.ar_sites == 2:
+            return self.ar_sampling_two_sites(n_sample)
+        elif self.ar_sites == 1:
+            return self.ar_sampling_one_sites(n_sample)
 
 from typing import Tuple
 
@@ -294,7 +465,7 @@ if __name__ == "__main__":
     device = "cpu"
     sorb = 8
     nele = 4
-    alpha = 2
+    alpha = 1
     fock_space = onv_to_tensor(get_fock_space(sorb), sorb)
     length = fock_space.shape[0]
     fci_space = onv_to_tensor(given_onstate(x=sorb, sorb=sorb, noa=nele // 2, nob=nele // 2), sorb)
@@ -302,7 +473,14 @@ if __name__ == "__main__":
     # random_order = random.sample(list(range(length)), length)
     # fock_space = fock_space[random_order]
     ar_rbm = RBMSites(
-        sorb, nele=nele, alpha=alpha, init_weight=0.005, symmetry=True, common_weight=True
+        sorb,
+        nele=nele,
+        alpha=alpha,
+        init_weight=0.005,
+        symmetry=True,
+        common_weight=True,
+        ar_sites=1,
+        activation_type="coslinear"
     )
     rnn = RNNWavefunction(
         sorb,
@@ -315,12 +493,12 @@ if __name__ == "__main__":
         device=device,
     )
     rbm = RBMWavefunction(sorb, alpha=alpha, init_weight=0.005, rbm_type="cos")
-    model = rnn
+    model = ar_rbm
     # x = torch.load("AR-RBM-H4-checkpoint.pth", map_location="cpu")
     # model.hidden_bias.data = x["model"]["module.hidden_bias"].to(device)
     # model.weights.data = x["model"]["module.weights"].to(device)
 
-    if True:
+    if False:
         model.zero_grad()
         print(fci_space.requires_grad)
         psi = model(fci_space[12].requires_grad_())
