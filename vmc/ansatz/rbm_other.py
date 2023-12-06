9b3cb748e9f7ef0fb205b819c13bfb56ac10af2d
BohanZhang <xb782053@gmail.com>
2023-12-06 22:15:12 +0800

    🌈 style: add sth printed

--- vmc/ansatz/rbm_other.py
+++ vmc/ansatz/rbm_other.py
@@ -9,6 +9,8 @@ from utils.public_function import multinomial_tensor, torch_lexsort
 from libs.C_extension import constrain_make_charts
 
 from vmc.ansatz.utils import OrbitalBlock
+
+
 class IsingRBM(nn.Module):
     __constants__ = ["num_visible"]
 
@@ -34,22 +36,29 @@ class IsingRBM(nn.Module):
         self.num_hidden = int(self.alpha * self.num_visible)
 
         self.hidden_bias = nn.Parameter(
-            torch.randn(self.num_hidden, dtype=self.param_dtype) * self.iscale
+            torch.randn(self.num_hidden, dtype=self.param_dtype, device=self.device) * self.iscale
         )
         self.weight_1 = nn.Parameter(
-            torch.randn(self.num_visible, self.num_hidden, dtype=self.param_dtype) * self.iscale
+            torch.randn(
+                self.num_visible, self.num_hidden, dtype=self.param_dtype, device=self.device
+            )
+            * self.iscale
         )
 
         if self.order >= 2:
             self.weight_2 = nn.Parameter(
                 torch.randn(
-                    self.num_hidden, self.num_visible, self.num_visible, dtype=self.param_dtype
+                    self.num_hidden,
+                    self.num_visible,
+                    self.num_visible,
+                    dtype=self.param_dtype,
+                    device=self.device,
                 )
                 * (self.iscale / 10)
             )
 
     def forward(self, input: Tensor):
-        vis_input = torch.tensor(input, dtype=self.param_dtype)
+        vis_input = torch.tensor(input, dtype=self.param_dtype, device=self.device)
         # print(vis_input)
         # print(vis_input.size())
         # print(torch.sum(vis_input, dim=-1))
@@ -87,12 +96,9 @@ class ARRBM(nn.Module):
         device="cpu",
         use_share_para: bool = False,
         normal: bool = True,
-        cut_0:bool = True,
+        cut_0: bool = True,
         # 添加相位所用
-        compute_phase = True,
-        common_linear: bool = False,
-        combine_amp_phase: bool = True,
-        phase_hidden_size: List[int] = [32, 32],
+        compute_phase=True,
         phase_use_embedding: bool = False,
         phase_hidden_activation: Union[nn.Module, Callable] = nn.ReLU,
         phase_bias: bool = True,
@@ -116,10 +122,10 @@ class ARRBM(nn.Module):
         self.num_visible = num_visible
         self.correct_size2 = correct_size
         if self.correct_size2 == None:
-            self.correct_size = int(self.num_visible//4) # 自动分配电子数为自选轨道数的一半
+            self.correct_size = int(self.num_visible // 4)  # 自动分配电子数为自选轨道数的一半
             # print(self.correct_size)
         else:
-            self.correct_size = self.correct_size2 # 也就是说我现在计算的是一个离子体系（带电体系）
+            self.correct_size = self.correct_size2  # 也就是说我现在计算的是一个离子体系（带电体系）
 
         # 定义参数之类型与大小
         self.param_dtype = param_dtype
@@ -135,21 +141,34 @@ class ARRBM(nn.Module):
                 self.weight_dim = self.num_visible * (1 + self.num_visible) // 2
         # 初始化参数
         # self.hidden_bias = nn.Parameter(
-        #     torch.randn(size=(self.num_hidden,), dtype=self.param_dtype) * self.iscale 
+        #     torch.randn(size=(self.num_hidden,), dtype=self.param_dtype) * self.iscale
         # )  # (n_h)
         # self.weight = nn.Parameter(
         #     torch.randn(size=(self.num_hidden, self.weight_dim), dtype=self.param_dtype)
-        #     * self.iscale 
+        #     * self.iscale
         # )  # (n_h,n_W)
         self.cut_0 = cut_0
         self.hidden_bias = nn.Parameter(
-            (torch.rand(size=(self.num_hidden,), dtype=self.param_dtype) * self.iscale - 0.5)*0.002
+            (
+                torch.rand(size=(self.num_hidden,), dtype=self.param_dtype, device=self.device)
+                * self.iscale
+                - 0.5
+            )
+            * 0.002
         )  # (n_h)
         self.weight = nn.Parameter(
-            (torch.rand(size=(self.num_hidden, self.weight_dim), dtype=self.param_dtype)
-            * self.iscale - 0.5)*0.002
+            (
+                torch.rand(
+                    size=(self.num_hidden, self.weight_dim),
+                    dtype=self.param_dtype,
+                    device=self.device,
+                )
+                * self.iscale
+                - 0.5
+            )
+            * 0.002
         )  # (n_h,n_W)
-        # 是否归一化 
+        # 是否归一化
         self.normal = normal
         self.factory_kwargs = {"device": self.device, "dtype": self.param_dtype}
         self.use_unique = True
@@ -160,7 +179,7 @@ class ARRBM(nn.Module):
         if phase_use_embedding:
             raise NotImplementedError(f"Phases layer embedding will be implemented in future")
         self.n_out_phase = n_out_phase
-        self.phase_hidden_size = phase_hidden_size
+        self.phase_hidden_size = [self.num_hidden * 2, self.num_hidden * 2]
         self.phase_hidden_activation = phase_hidden_activation
         self.phase_use_embedding = phase_use_embedding
         self.phase_bias = phase_bias
@@ -183,11 +202,28 @@ class ARRBM(nn.Module):
             self.phase_layers.append(phase_i.to(self.device))
             self.phase_layers = nn.ModuleList(self.phase_layers)
 
+    def extra_repr(self) -> str:
+        s = "AR-RBM -- activate function: " + str(self.activation) + "\n"
+        s += "The spin is used: " + str(self.spin) + "\n"
+        s += (
+            "sym is used: "
+            + str(self.use_correct_size)
+            + " , and the correct number:"
+            + str(self.correct_size)
+            + "\n"
+        )
+        s += "alpha factor: " + str(self.alpha) + "\n"
+        s += "The number of vis_number: " + str(self.num_visible) + "\n"
+        s += "Normlization: " + str(self.normal) + "\n"
+        s += "Share paras: " + str(self.use_share_para)
+        return s
+
     def trans01(self, input):
         if input.numel() > 0:
             if torch.min(input) == -1:
                 input = (1 + input) / 2
         return input
+
     # 这两个函数只能判断是否有对称性问题，但是无法保证对称性迭代
     def check_Sz(self, input):  # 用来检查是否是总自旋投影为0
         input = self.trans01(input)
@@ -205,9 +241,10 @@ class ARRBM(nn.Module):
             vis_size = torch.sum(input, dim=-1)  # spinless
         target = torch.where(vis_size != correct_size, torch.tensor(0), torch.tensor(1))
         return target
-    def cond_psi(self, vis_input:Tensor, i:int, state:bool = True):
+
+    def cond_psi(self, vis_input: Tensor, i: int, state: bool = True):
         if self.spin:
-                j = 2 * i
+            j = 2 * i
         # 处理 W
         if self.use_share_para:
             if self.spin:
@@ -216,9 +253,7 @@ class ARRBM(nn.Module):
                 W_i = self.weight[:, : i + 1]  # 如果共享参数那么不用对 W 进行切片，直接取前 i+1 列就可以 (n_h,i+1)
         else:
             if self.spin:
-                W_i = self.weight[
-                    :, int((2 + j) * j / 4) : int((4 + j) * (2 + j) / 4)
-                ]  # (n_h,i+2)
+                W_i = self.weight[:, int((2 + j) * j / 4) : int((4 + j) * (2 + j) / 4)]  # (n_h,i+2)
             else:
                 W_i = self.weight[
                     :, int(i * (i + 1) / 2) : int(i * (i + 1) / 2 + i + 1)
@@ -229,7 +264,7 @@ class ARRBM(nn.Module):
             W_i_cur = W_i[:, -2:]  # (n_h,2)
             vis_i = vis_input[:, : j + 2]  # (N,i+2) 以上矩阵对应着 vis_input 每一行前 i+2 个元素的矢量
             vis_i_pre = vis_i[:, :j]  # (N,i)
-            vis_cur = vis_i[:, -2:] # (n_h,2)
+            vis_cur = vis_i[:, -2:]  # (n_h,2)
         else:
             W_i_pre = W_i[:, :i]  # (n_h,i) 用于计算条件概率 除去最后一组维度的前面 i 列
             W_i_cur = W_i[:, i]  # (n_h,1) 新来的 1 列
@@ -246,11 +281,11 @@ class ARRBM(nn.Module):
         if self.spin:
             atmp_pre = atmp_pre.unsqueeze(-1).expand(-1, -1, 4)  # (N, n_h, 4)
             all = torch.tensor([[0, 0], [1, 0], [0, 1], [1, 1]], **self.factory_kwargs)  # (4, 2)
-            cur_cond = torch.einsum("nj,ij->ni",W_i_cur, all) # (n_h, 2) * (2, 4) -> (n_h, 4)
+            cur_cond = torch.einsum("nj,ij->ni", W_i_cur, all)  # (n_h, 2) * (2, 4) -> (n_h, 4)
             # cur_cond = torch.matmul(W_i_cur, all.T) # (n_h, 2) * (2, 4) -> (n_h, 4)
         else:
             atmp_pre = atmp_pre.unsqueeze(-1).expand(-1, -1, 2)  # (N, n_h, 2)
-            all = torch.tensor([0, 1], **self.factory_kwargs)  
+            all = torch.tensor([0, 1], **self.factory_kwargs)
             cur_cond = torch.einsum("x,i->xi", W_i_cur, all)  # (n_h, 1) * 2 -> (n_h, 2)
         atmp_cur = cur_cond.unsqueeze(0).expand(vis_input.shape[0], -1, -1)  # (N, n_h, 2)
         atmp = atmp_pre + atmp_cur
@@ -261,11 +296,16 @@ class ARRBM(nn.Module):
         if state:
             if self.use_correct_size:
                 if i == 0:
-                    vis_sym = torch.zeros(vis_i.shape[0], self.batch_add//2, dtype=self.param_dtype)
-                    self.vis_sym = vis_i
+                    vis_sym = torch.zeros(
+                        vis_i.shape[0],
+                        self.batch_add // 2,
+                        dtype=self.param_dtype,
+                        device=self.device,
+                    )
+                    self.vis_sym = vis_i.to(self.device)
                 else:
                     vis_sym = self.vis_sym
-                    self.vis_sym = vis_i # 记录这次循环的输入，以确保下个循环生成新两列的对称性
+                    self.vis_sym = vis_i  # 记录这次循环的输入，以确保下个循环生成新两列的对称性
                 lower_up = self.correct_size - self.num_visible // 2 + i
                 lower_down = self.correct_size - self.num_visible // 2 + i
                 # print(vis_sym)
@@ -281,15 +321,18 @@ class ARRBM(nn.Module):
                     [activations_occ0, activations_unocc0, activations_occ1, activations_unocc1],
                     dim=1,
                 )
+                # sym_index = sym_index.to(self.device)
                 sym_index = (sym_index * torch.tensor([1, 2, 4, 8], device=self.device)).sum(dim=1)
                 sym_index = constrain_make_charts(sym_index)
                 condwf.mul_(sym_index)
                 condwf = F.normalize(condwf, dim=1, eps=1e-14)
-            ind = torch.tensor([1,2], dtype=torch.float64) # (1,2)
+            ind = torch.tensor([1, 2], dtype=torch.float64, device=self.device)  # (1,2)
             index = (vis_cur * ind).sum(dim=1).long()
             # index = torch.einsum("ni,i->n",vis_cur,ind).to(torch.int64) # (N, 2) * (1, 2)  -> (N, 1)
-            index = torch.nn.functional.one_hot(index, num_classes=4).to(torch.float64) # (N, 1) -> (N, 4)
-            psi_1 = (condwf * index).sum(dim=-1) # (N, 4) -> (N)
+            index = torch.nn.functional.one_hot(index, num_classes=4).to(
+                torch.float64
+            )  # (N, 1) -> (N, 4)
+            psi_1 = (condwf * index).sum(dim=-1)  # (N, 4) -> (N)
             # psi_1 = torch.prod(activation, dim=-1)  # prod along n_h direction -> (N)
             return psi_1
         else:
@@ -322,11 +365,13 @@ class ARRBM(nn.Module):
         else:
             self.vis_dim = self.num_visible
         for i in range(self.vis_dim):
-            psi_1= self.cond_psi(vis_input,i)
+            psi_1 = self.cond_psi(vis_input, i)
             vis_output = vis_output * psi_1
-        
+
         if self.compute_phase:
-            phase_input = vis_input.masked_fill(vis_input == 0, -1).double().squeeze(1)  # (nbatch, 2)
+            phase_input = (
+                vis_input.masked_fill(vis_input == 0, -1).double().squeeze(1)
+            )  # (nbatch, 2)
             phase_i = self.phase_layers[0](phase_input)
             if self.n_out_phase == 1:
                 phase = phase_i.view(-1)
@@ -335,11 +380,11 @@ class ARRBM(nn.Module):
             vis_output = torch.complex(torch.zeros_like(phase), phase).exp() * vis_output
         return vis_output
 
-    def joint_next_sample(self, tensor: Tensor) -> Tensor: # 用以查看产生下一个样本的可能态
-        batch_add = self.batch_add 
-        orbital_add = self.batch_add//2
+    def joint_next_sample(self, tensor: Tensor) -> Tensor:  # 用以查看产生下一个样本的可能态
+        batch_add = self.batch_add
+        orbital_add = self.batch_add // 2
         if self.spin:
-            empty = torch.tensor([[0, 0]], **self.factory_kwargs) # (1, 2)
+            empty = torch.tensor([[0, 0]], **self.factory_kwargs)  # (1, 2)
             full = torch.tensor([[1, 1]], **self.factory_kwargs)
             a = torch.tensor([[1, 0]], **self.factory_kwargs)
             b = torch.tensor([[0, 1]], **self.factory_kwargs)
@@ -351,11 +396,13 @@ class ARRBM(nn.Module):
         nbatch, k = tuple(tensor.shape)
         x = torch.empty(int(nbatch * batch_add), int(k + orbital_add), **self.factory_kwargs)
         for i in range(batch_add):
-            x[i * nbatch : (i + 1) * nbatch, (0 - orbital_add):] = maybe[i].repeat(nbatch, 1) # (nbatch, 2) -> # (batch_add * nbatch, 2)
+            x[i * nbatch : (i + 1) * nbatch, (0 - orbital_add) :] = maybe[i].repeat(
+                nbatch, 1
+            )  # (nbatch, 2) -> # (batch_add * nbatch, 2)
 
-        x[:, :(0 - orbital_add)] = tensor.repeat(batch_add, 1)
+        x[:, : (0 - orbital_add)] = tensor.repeat(batch_add, 1)
         return x
-        
+
     def ar_sampling(self, n_sample: int) -> Tuple[Tensor, Tensor, Tensor]:
         sample_counts = torch.tensor([n_sample], device=self.device, dtype=torch.int64)
         sample_unique = torch.ones(1, 0, device=self.device, dtype=torch.int64)
@@ -364,19 +411,19 @@ class ARRBM(nn.Module):
             phase = torch.zeros(1, **self.factory_kwargs)
         else:
             phase: Tensor = None
-        if self.spin: 
+        if self.spin:
             vis_dim = self.num_visible // 2
         else:
             vis_dim = self.num_visible
         for i in range(vis_dim):
-            x0 = sample_unique 
-            y0 = self.cond_psi(x0, i,state=False)  # (n_unique, 4)
+            x0 = sample_unique
+            y0 = self.cond_psi(x0, i, state=False)  # (n_unique, 4)
             if self.use_correct_size:
                 alpha = self.correct_size
                 beta = self.correct_size
                 baseline_up = alpha - self.num_visible // 2
                 baseline_down = beta - self.num_visible // 2
-                lower_up = baseline_up + i 
+                lower_up = baseline_up + i
                 lower_down = baseline_down + i
                 n_unique = sample_unique.size(0)
                 num_up = sample_unique[:, ::2].sum(dim=1)
@@ -404,7 +451,9 @@ class ARRBM(nn.Module):
             sample_unique = self.joint_next_sample(sample_unique)[idx_count]
 
             # update wavefunction value that is similar to updating sample-unique
-            amp = torch.mul(wf_value.unsqueeze(1).repeat(1, self.batch_add), y0).T.flatten()[idx_count]
+            amp = torch.mul(wf_value.unsqueeze(1).repeat(1, self.batch_add), y0).T.flatten()[
+                idx_count
+            ]
 
             if self.compute_phase:
                 wf = torch.complex(torch.zeros_like(phase), phase).exp() * amp
