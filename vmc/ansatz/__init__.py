9337774181b75f5ee8b9d4205bb3907b309cc7de
BohanZhang <xb782053@gmail.com>
2024-01-15 20:37:29 +0800

    🐞 fix(vmc/ansatz):Change the location of mps_transformer

--- vmc/ansatz/__init__.py
+++ vmc/ansatz/__init__.py
@@ -5,7 +5,7 @@ from .ar_rbm import RBMSites
 from .transformer.decoder import DecoderWaveFunction
 from .hybrid.hybrid import HybridWaveFunction
 from .hybrid.hqc import HybridQCWaveFunction
-from .mps_transformer import MPSdecoder
+from .transformer.mps_transformer import MPSdecoder
 
 try:
     from .mps import MPSWavefunction
