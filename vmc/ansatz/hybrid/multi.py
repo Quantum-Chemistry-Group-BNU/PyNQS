Fixed the DDP cannot synchronize gradient bug in MultiPsi

vmc/ansatz/hybrid/multi.py
@@ -43,10 +43,10 @@ class MultiPsi(AnsatzARBase):
     ) -> Tuple[Tensor, Tensor, Tensor]:
         return self.sample.ar_sampling(n_sample, min_batch, min_tree_height, use_dfs_sample)
 
-    def forward(self, x: Tensor):
-        return self.sample(x)
+    def forward(self, x: Tensor) -> Tensor:
+        return self.sample(x) * self.extra(x)
 
     @torch.no_grad
-    def call(self, x):
+    def call(self, x) -> Tensor:
         device = self.sample.device
         return torch.ones(x.size(0), device=device, dtype=torch.double) * 1.0

vmc/grad/energy_grad.py
@@ -200,7 +200,7 @@ def new_grad(
     AD_MAX_DIM: int = -1,
 ):
     """
-    2 Re<[(ln psi_n* + ln f_n*)(eloc_new - E * extra_phi_pow)]>
+    loss = 2 Re<[(ln psi_n* + ln f_n*)(eloc_new - E * extra_phi_pow)]>
     """
     device = states.device
     dim = states.size(0)
@@ -218,23 +218,22 @@ def new_grad(
     def batch_loss_backward(begin: int, end: int) -> None:
         nonlocal loss_sum
         state = states[begin: end].requires_grad_()
-        log_psi = nqs.module.sample(state).to(dtype).log()
-        log_f = nqs.module.extra(state).to(dtype).log()
+        log_psi_f = nqs(state).to(dtype).log()
 
         state_prob_batch = state_prob[begin:end].real.to(dtype)
         eloc_batch = eloc[begin:end].to(dtype)
         extra_psi_pow_batch = extra_psi_pow[begin: end].to(dtype)
 
-        if torch.any(torch.isnan(log_psi)):
+        if torch.any(torch.isnan(log_psi_f)):
             raise ValueError(f"There are negative numbers in the log-psi, please use complex128")
 
-        loss1 = log_psi.conj() + log_f.conj()
+        loss1 = log_psi_f.conj()
         loss2 = eloc_batch - e_total * extra_psi_pow_batch
         loss = 2 * (loss1 * loss2 * state_prob_batch).sum().real
         loss.backward()
         loss_sum += loss.detach()
 
-        del state_prob_batch, log_psi, loss
+        del state_prob_batch, log_psi_f, loss
 
     with MemoryTrack(device) as track:
         begin = 0

vmc/sample.py
@@ -362,7 +362,6 @@ class Sampler:
             sample_prob = sample_prob[idx]
         else:
             ci_space = self.ci_space
-        # TODO: remove
         ci_space_rank = scatter_tensor(ci_space, self.device, torch.uint8, self.world_size)
         eloc, sloc, _ = self.calculate_energy(
             ci_space_rank,
@@ -706,7 +705,6 @@ class Sampler:
             # XXX: unique_rank split merge_unique when broadcast to all-rank,
             # this maybe efficiency than scatter->broadcast
             merge_unique = broadcast_tensor(merge_unique, self.device, torch.uint8, master_rank=0)
-            # breakpoint()
             wf_value_unique = broadcast_tensor(
                 wf_value_unique, self.device, self.dtype, master_rank=0
             )
@@ -1002,7 +1000,11 @@ class Sampler:
 
         t0 = time.time_ns()
         with torch.no_grad():
-            psi_rank = self.ansatz_batch(ci_space_rank, self.nqs, fp_batch)
+            if self.use_multi_psi:
+                model = self.nqs.module.sample
+            else:
+                model = self.nqs
+            psi_rank = self.ansatz_batch(ci_space_rank, model, fp_batch)
         t1 = time.time_ns()
         if self.rank == 0:
             logger.info(f"Rank-psi: {(t1-t0)/1.0e9:.3E} s", master=True)