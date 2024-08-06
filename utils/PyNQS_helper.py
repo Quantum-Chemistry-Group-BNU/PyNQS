fix variance bug in PyNQS_helper.py

utils/PyNQS_helper.py
@@ -59,20 +59,19 @@ def read_time_from_log(filename: str, verbose: bool = False, save_file: bool =Fa
                 line = line.split()
                 n_iter +=1
                 eloc_mean.append(float(line[2]))
-                eloc_var.append(float(line[-1].replace("]", "")))
+                eloc_var.append(float(line[-2]))  # ± std
             elif line.startswith("<S-S+>"):
                 # <eloc-mean> <NQS|H|NQS>
                 # E_total = -97.9124353057 ± 9.438E-06 [σ² = 8.908E+01]
-                # eloc_mean.append(float(line.split()[2]))
                 line = line.split()
                 spin_mean.append(float(line[2]))
-                spin_var.append(float(line[-1].replace("]", "")))
+                spin_var.append(float(line[-2]))
             elif line.startswith("<f(n)²>"):
                 # <f(n)²> = 0.000030373 ± 1.496E-07 [σ² = 2.239E-08]
                 # MPS-RNN + RBM, f(n) is RBM
                 line = line.split()
                 fn_mean.append(float(line[2]))
-                fn_var.append(float(line[-1].replace("]", "")))
+                fn_var.append(float(line[-2]))  # ± std
             elif re_grad.search(line):
                 # auto-grad, update param
                 # Calculating grad: 2.221E-02 s, update param: 4.656E-04 s
@@ -187,6 +186,10 @@ def read_time_from_log(filename: str, verbose: bool = False, save_file: bool =Fa
     else:
         fn_var = np.asarray(fn_var)[:n_iter]
         fn_mean = np.asarray(fn_mean)[:n_iter]
+
+    fn_var = np.power(fn_var, 2)
+    spin_var = np.power(spin_var, 2)
+    eloc_var = np.power(eloc_var, 2)
 
     # only-sampling
     if only_sampling: